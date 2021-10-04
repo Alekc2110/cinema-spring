e927b5a555d1169bf0ff13347e948ba5becb7f75
Oleksandr Korshunovych <korsunonica@gmail.com>
2021-10-04 17:33:23 +0300

    constants removed to Constants

---
+++ src/main/java/com/my/booking/cinema/controller/Constants.java
@@ -0,0 +1,48 @@
+package com.my.booking.cinema.controller;
+
+public final class Constants {
+    public static final String UPDATE_SUCCESS = "successUpdate";
+    public static final String UPDATE_MS_SUCCESS = "successUpdateMS";
+    public static final String ADD_SUCCESS_MS = "successAddMS";
+    public static final String MOVIE_EDIT = "movieEdit";
+    public static final String MOVIE_SES_EDIT = "movieSesEdit";
+    public static final String DELETE_SUCCESS = "successDel";
+    public static final String DELETE_MS_SUCCESS = "successDelMS";
+    public static final String PERCENTAGE_BOOKED_SEATS = "percentage";
+    public static final String MOVIE_S_LIST_ATTRIBUTE = "moviesSesList";
+    public static final String MOVIE_ID = "movieId";
+    public static final String MOVIE_TITLE = "movieTitle";
+    public static final String TOTAL_ELEMENTS = "totalElements";
+    public static final String DATA = "data";
+    public static final String RECORD_PER_PAGE_AT = "size";
+    public static final String PAGE_NUMBER_AT = "number";
+    public static final String PAGE_NUMBERS_ATTRIBUTE = "totalPages";
+    public static final int PERCENTAGE_100 = 100;
+    public static final int HALL_CAPACITY = 50;
+    public static final String PASS_ERROR = "passwordError";
+    public static final String USER_SAVE_ERROR = "usernameError";
+    public static final String ACTIVE_MOVIES_LIST = "activeMovies";
+    public static final String MOVIE = "movie";
+    public static final String ACTIVE_MOVIE = "activeMovie";
+    public static final String MS_LIST_BY_MOVIE = "movieSessions";
+    public static final String ORDER_SESSION_ATTR = "order";
+    public static final String MOVIE_ATTR = "movie";
+    public static final String MOVIE_SESSION_ATTR = "movieSession";
+    public static final String SEATS_LIST_ATTR = "allSeats";
+    public static final String ALREADY_BOOKED_SEAT = "bookedSeats";
+    public static final String SAVE_ORDER_FALSE = "orderFalse";
+    public static final String USER = "user";
+    public static final String TICKET_LIST_ATTRIBUTE = "ticketList";
+    public static final String SORT_OPTION = "option";
+    public static final String SORT_BY_TITLE = "title";
+    public static final String SORT_BY_DATE = "date";
+    public static final String SORT_BY_TIME = "time";
+    public static final String MOVIE_SES_LIST_TODAY = "movieListNow";
+    public static final String MOVIE_SES_LIST_TOM = "movieListTomorrow";
+    public static final String MOVIE_SES_LIST_NOW2 = "movieListNowPlTwo";
+    public static final String MOVIE_SES_LIST_NOW3 = "movieListNowPlThree";
+    public static final String MOVIE_SES_LIST_NOW4 = "movieListNowPlFour";
+    public static final String MOVIE_SES_LIST_NOW5 = "movieListNowPlFive";
+    public static final String MOVIE_SES_LIST_NOW6 = "movieListNowPlSix";
+
+}
